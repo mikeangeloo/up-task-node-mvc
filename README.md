275c06ab2ffc5111179774b0ecfdbe3de3d20de2
mikeangeloo <mikeangeloo87@outlook.com>
2019-09-08 16:25:03 -0500

    se agrega formulario y rutas para agregar nuevo proyectos

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-###UP TASK
-####Node Project, making with:
+### UP TASK
+#### Node Project, making with:
 - MVC patron
 - Node express
 - Pug html engine template
--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -3,3 +3,31 @@ exports.proyectosHome = (req, res) => {
     nombrePagina : 'Proyectos'
   });
 };
+
+exports.formularioProyecto = (req, res) => {
+  res.render('nuevoProyecto', {
+    nombrePagina: 'Nuevo Proyecto'
+  })
+};
+
+exports.nuevoProyecto = (req, res) => {
+  // console.log(req.body);
+  // validar que tengamos algo en el input
+  const { nombre } = req.body;
+
+  let errores = [];
+
+  if (!nombre) {
+    errores.push({'texto': 'Agrega un nombre al proyecto'})
+  }
+  // si hay errores
+  if(errores.length > 0){
+    res.render('nuevoProyecto', {
+      nombrePagina: 'Nuevo Proyecto',
+      errores
+    })
+  }else {
+    // no hay errores
+    // insertar en la BD
+  }
+};
